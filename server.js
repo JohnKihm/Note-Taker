61cc27946dc8ac0f3506bbc0c5c4e4402582a30d
JohnKihm <john.a.kihm@gmail.com>
2024-06-17 02:52:16 -0500

    added /api/notes get path

--- server.js
+++ server.js
@@ -6,9 +6,26 @@ const PORT = process.env.port || 3001;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.err(err);
+            return;
+        } else {
+            const parsedNotes = JSON.parse(data);
+            res.json(parsedNotes);
+        }
+    })
+});
 
 app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
