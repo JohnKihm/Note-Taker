9972fec4d3e715bf5d9bc1990af35be43ada42f3
JohnKihm <john.a.kihm@gmail.com>
2024-06-16 19:18:17 -0500

    added * get route and app.listen

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 const express = require('express');
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+const PORT = process.env.port || 3001;
 
 const app = express();
+
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () =>
+console.log(`App listening at http://localhost:${PORT}`)
+);
