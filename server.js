feeee452bec0de96a3dd1c359e9ad63d00b1fc39
JohnKihm <john.a.kihm@gmail.com>
2024-06-16 19:48:52 -0500

    added /notes get path

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const PORT = process.env.port || 3001;
 
 const app = express();
 
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
 app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
